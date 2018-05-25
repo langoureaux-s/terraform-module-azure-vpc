ae9d7e11fc6ef02cc331ddf97d19199531f72c23
Sebastien Langoureaux <sebastien.langoureaux@sihm.fr>
2018-05-25 08:40:26 +0000

    Remove bad output

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "public_dns_list" {
-  value = "${join(",", azurerm_public_ip.*.fqdn)}"
-}
-
-output "private_dns_list" {
-  value = "${join(",", azurerm_network_interface.*.internal_fqdn)}"
-}
