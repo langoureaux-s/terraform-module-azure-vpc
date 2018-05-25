dc821b84b74dc208763f0693e268750405e287e2
Sebastien Langoureaux <sebastien.langoureaux@sihm.fr>
2018-05-25 13:15:28 +0000

    Add output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,19 @@ output "resource_group_id" {
 output "resource_group_name" {
   value = "${azurerm_resource_group.rg.name}"
 }
+
+output "virtual_network_id" {
+  value = "${azurerm_virtual_network.vpc.id}"
+}
+
+output "virtual_network_name" {
+  value = "${azurerm_virtual_network.vpc.name}"
+}
+
+output "subnet_id" {
+  value = "${azurerm_subnet.subnet.id}"
+}
+
+output "subnet_name" {
+  value = "${azurerm_subnet.subnet.name}"
+}
